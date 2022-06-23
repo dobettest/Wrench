2473078ca53ed8d6f490445daf4fc276c2609b2c
genu.liu <genu@cloudwise.com>
2022-06-23 22:26:34 +0800

    fixed #WREN-19 修复optionalEntry is not a function

--- scaffold/webpack/aegis.js
+++ scaffold/webpack/aegis.js
@@ -1,17 +1,38 @@
-const { expandConfig } = require("../utils")
-module.exports = () => {
-    const { id, uin, reportApiSpeed, reportAssetSpeed, spa } = expandConfig('aegisOptions', {});
+const { validate } = require('schema-utils');
+
+const schema = {
+    type: 'object',
+    properties: {
+        id: {
+            type: 'string',
+        },
+        // uin: 'xxx', // 用户唯一 ID（可选）
+        reportApiSpeed: {
+            type: 'boolean',
+        }, // 接口测速
+        reportAssetSpeed: {
+            type: 'boolean',
+        }, // 静态资源测速
+        spa: {
+            type: 'boolean',
+        } // spa 应用页面跳转的时候开启 pv 计算
+    },
+};
+module.exports = function (source) {
+    const options = this.getOptions();
+    validate(schema, options);
+    const { id, uin = '', reportApiSpeed, reportAssetSpeed, spa } = options;
     const template = `
     import Aegis from 'aegis-web-sdk';
-
+    ${source}
     const aegis = new Aegis({
-    id: ${id}, // 应用ID，即上报ID
-    uin: ${uin}, // 用户唯一 ID（可选）
+    id: '${id}', // 应用ID，即上报ID
+    // uin: '${uin}', // 用户唯一 ID（可选）
     reportApiSpeed: ${reportApiSpeed}, // 接口测速
     reportAssetSpeed: ${reportAssetSpeed}, // 静态资源测速
     spa: ${spa} // spa 应用页面跳转的时候开启 pv 计算
     })
-    window.AegisClient = aegis;//其他页面直接使用AegisClient就可以进行监控
+    // window.AegisClient = aegis;//其他页面直接使用AegisClient就可以进行监控
     `
     return template;
 }
